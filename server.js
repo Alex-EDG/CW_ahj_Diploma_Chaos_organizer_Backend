36f6962134bf8cbf6ad95d240816cb7964e2958d
Alex-EDG <AlexEDG75@GMail.Com>
2026-05-03 01:21:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const rawBody = require("raw-body");
 const Database = require("./components/dataBase");
 const FileStorage = require("./components/fileStorage");
 const fs = require("fs");
-const dataConfig = fs.readFileSync("./config.json", "utf8");
+const dataConfig = fs.readFileSync("config.json", "utf8");
 const config = JSON.parse(dataConfig);
 const emitter = new Emitter();
 
